d3d59b595919a4cc8e1862c6c791fd53a825b0d6
BobbieO <bobbie2504@msn.com>
2016-06-01 16:49:27 -0500

    Changed navbar links to route to proper place using new syntax with home controller.

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -23,25 +23,25 @@
     <header>
         <!-- Dropdown Structure -->
         <ul id="dropdownJava" class="dropdown-content">
-          <li><a class="purple-text" href="/wizard.php">Wizard Vs Dragon</a></li>
-          <li><a class="purple-text" href="/catchabutterfly.php">Catch A Butterfly</a></li>
-          <li><a class="purple-text" href="/simpleSimon.php">Simple Simon</a></li>
-          <li><a class="purple-text" href="/weathermap.php">Weather Map</a></li>
-          <li><a class="purple-text" href="/basicCalculator.php">Basic Calculator</a></li>
+          <li><a class="purple-text" href="{{{action('HomeController@showWizard')}}}">Wizard Vs Dragon</a></li>
+          <li><a class="purple-text" href="{{{action('HomeController@showButterfly')}}}">Catch A Butterfly</a></li>
+          <li><a class="purple-text" href="{{{action('HomeController@showSimpleSimon')}}}">Simple Simon</a></li>
+          <li><a class="purple-text" href="{{{action('HomeController@showWeatherMap')}}}">Weather Map</a></li>
+          <li><a class="purple-text" href="{{{action('HomeController@showCalculator')}}}">Basic Calculator</a></li>
           <li class="divider"></li>
 
-          <li><a class="purple-text" href="/randomQuestGen.php">Random Quest Generator</a></li>
+          <li><a class="purple-text" href="{{{action('HomeController@showRandomQuest')}}}">Random Quest Generator</a></li>
           <li class="divider"></li>
 
-          <li><a class="purple-text" href="/index.php">Future Projects</a></li>
+          <li><a class="purple-text" href="{{{action('HomeController@showHome')}}}">Future Projects</a></li>
         </ul>
 
         <nav>
             <div class="nav-wrapper grey darken-3">
-                <a href="/index.php" class="brand-logo">BHO</a>
+                <a href="{{{action('HomeController@showHome')}}}" class="brand-logo">BHO</a>
                 <ul class="text right"> 
-                    <li><a href="/index.php">Contact Me</a></li>
-                    <li><a href="/index.php">Resume</a></li>
+                    <li><a href="{{{action('HomeController@showHome')}}}">Contact Me</a></li>
+                    <li><a href="{{{action('HomeController@showHome')}}}">Resume</a></li>
                     <!-- Dropdown Trigger -->
                     <li><a class="dropdown-button" href="#!" data-activates="dropdownJava">Projects<i class="material-icons right">arrow_drop_down</i></a></li>
                 </ul>
