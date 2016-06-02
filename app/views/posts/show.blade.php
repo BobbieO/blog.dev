722b7040e0aaccbf45d46f678ec0f3bc6308be23
BobbieO <bobbie2504@msn.com>
2016-06-02 16:18:01 -0500

    Edited styling.

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -13,10 +13,10 @@
                 <h1>Bobbie's Blog</h1>
             </div>
         </div>
-        
+
         <div class="row">
             <div class="col sm8">
-                <h2>{{{$post->title}}}</h2>
+                <h3>{{{$post->title}}}</h3>
                 <p>{{{$post->content}}}</p>
             </div>
         </div>
