6eb5083414a2eebe219cc7f36009a33fe236d685
BobbieO <bobbie2504@msn.com>
2016-06-06 11:27:45 -0500

    Removed contact form, added supporting styling.

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -12,7 +12,7 @@
 @stop
 
 @section('content')
-    <main>
+<main>
     <div class="container">
         <div class="center-align">
             <p class="bigger">Hello and welcome to my site!</p>
@@ -26,60 +26,33 @@
 
         <hr>
 
-        <div class="">
-            <h4>Contact Me</h4>
+        <div>
+            <h3>Contact Me</h3>
             <a id="contact"></a>
             <div class="row social">
                 <div class="col s6">
                     <a href="https://twitter.com/bobbieo134" class="twitter-follow-button" data-show-count="false">Follow @bobbieo134</a>
                 </div>
+
                 <div class="col s6">
-                <a href="https://www.linkedin.com/in/bobbieo134"><img src="/img/linkedin-32-21.png"></a>
+                    <a href="https://www.linkedin.com/in/bobbieo134"><img src="/img/linkedin-32-21.png"></a>
                 </div>
             </div>
 
-            <div class="row">
-                <form class="col s12">
-                    <div class="row">
-                        <div class="input-field col s6">
-                            <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-                            <label for="first_name">First Name</label>
-                        </div>
-                        <div class="input-field col s6">
-                            <input id="last_name" type="text" class="validate">
-                            <label for="last_name">Last Name</label>
-                        </div>
-                    </div>
+            <div class="row social">
+                <div class="col s6">
+                    <p>Email will go here</p>
+                </div>
 
-                    <div class="row">
-                        <div class="input-field col s6">
-                            <input id="company" type="text" class="validate">
-                            <label for="company">Company</label>
-                        </div>
-                        <div class="input-field col s6">
-                            <input id="email" type="email" class="validate">
-                            <label for="email">Email</label>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <textarea id="message" class="materialize-textarea"></textarea>
-                            <label for="message">Message</label>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col s12">
-                            <button type="submit" class="btn waves-effect waves-light">Send Email</button>
-                        </div>
-                    </div>
-                </form>
+                <div class="col s6">
+                    <p>github</p>
+                </div>
             </div>
+         
         </div>
     <a href="#top">Return to top</a>
     <div>
-    </main>
+</main>
 @stop
 
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
