9b09b156613162a1769edf6e0f334b2c8381a257
BobbieO <bobbie2504@msn.com>
2016-06-05 20:30:58 -0500

    Added border radius to buttons. Added images for skills on resume page.

--- app/views/resume.blade.php
+++ app/views/resume.blade.php
@@ -7,13 +7,71 @@
 @section('content')
    
 <main>
-    <div>
-
-
-
-
-
+<div class="container">
+    <div class="row">
+        <div class="col s12 center-align">
+            <h3>Skills and Resume</h3>
+        </div>
     </div>
+
+    <div class="row">
+        <div class="col s12 center-align">
+            <button type="submit" class="btn waves-effect waves-light">Download Resume</button>
+        </div>
+    </div>
+
+    <div class="row valign-wrapper">
+        <div class="col s3">
+            <img src="/img/html5.png">
+        </div>
+        <div class="col s3">
+            <img src="/img/CSS3.png">
+        </div>
+        <div class="col s3">
+            <img src="/img/js.jpeg">
+        </div>
+        <div class="col s3">
+            <span class="valign"><img src="/img/jquery.png"></span>
+        </div>
+    </div>
+
+    <div class="row valign-wrapper">
+        <div class="col s3">
+            <span class="valign"><img src="/img/mysql.png"></span>
+        </div>
+        <div class="col s3">
+            <img src="/img/php.jpeg">
+        </div>
+        <div class="col s3">
+            <img src="/img/laravel.png">
+        </div>
+        <div class="col s3">
+            <img src="/img/sequelpro.jpeg">
+        </div>
+    </div>
+
+    <div class="row valign-wrapper">
+        <div class="col s3">
+            <img src="/img/gitHub.png">
+        </div>
+        <div class="col s3">
+            <img src="/img/sublime3.jpg">
+        </div>
+        <div class="col s3">
+            <img src="/img/bootstrap.jpeg">
+        </div>
+        <div class="col s3">
+            <img src="/img/materialize.jpeg">
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="center-align">
+            <img src="/img/pixelmator.jpeg">
+        </div>
+    </div>
+
+</div>
 </main>
 
 @stop
