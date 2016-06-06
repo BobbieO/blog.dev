3ef465e47fbbccb538b3d097192611ee7c0da751
BobbieO <bobbie2504@msn.com>
2016-06-06 12:01:51 -0500

    Added custom 404 page, image, and css.

---
+++ app/views/errors/missing.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.master')
+
+@section('title')
+    Ooops!
+@stop
+
+@section('content')
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col s12 center-align">
+                    <h1>Ooops!</h1>
+                        <p class="bigger text">Something went wrong with your request.</p>
+                        <img class="image" src="/img/oops.jpeg">
+                </div>
+            </div>
+        </div>
+    </main>
+@stop
