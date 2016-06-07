3f2f3fa1d52c128247ce22a17ca14dd932590d33
BobbieO <bobbie2504@msn.com>
2016-06-07 18:02:06 -0500

    Added mobile view to home navbar, added styling.

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -23,29 +23,37 @@
     <header>
         <!-- Dropdown Structure -->
         <ul id="dropdownJava" class="dropdown-content">
-          <li><a class="purple-text" href="{{{action('HomeController@showWizard')}}}">Wizard Vs Dragon</a></li>
-          <li><a class="purple-text" href="{{{action('HomeController@showButterfly')}}}">Catch A Butterfly</a></li>
-          <li><a class="purple-text" href="{{{action('HomeController@showBlog')}}}">Blog</a></li>
-
-          <!-- Need to link etc -->
-          <!-- <li><a class="purple-text" href="#">Put Adlister Here</a></li> -->
-          
-          <li class="divider"></li>
-
-          <li><a class="purple-text" href="{{{action('HomeController@showMore')}}}">More Projects</a></li>
+            <li><a class="purple-text" href="{{{action('HomeController@showWizard')}}}">Wizard Vs Dragon</a></li>
+            <li><a class="purple-text" href="{{{action('HomeController@showButterfly')}}}">Catch A Butterfly</a></li>
+            <li><a class="purple-text" href="{{{action('HomeController@showBlog')}}}">Blog</a></li>
+            <!-- Need to link etc -->
+            <!-- <li><a class="purple-text" href="#">Put Adlister Here</a></li> -->
+            <li class="divider"></li>
+            <li><a class="purple-text" href="{{{action('HomeController@showMore')}}}">More Projects</a></li>
         </ul>
         
         <nav>
             <div class="nav-wrapper grey darken-3">
                 <a href="{{{action('HomeController@showHome')}}}" class="brand-logo">BHO</a>
-                <ul class="text right"> 
+                <a href="#" data-activates="mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+                <ul class="text right hide-on-med-and-down"> 
                     <li><a href="/home#contact">Contact Me</a></li>
                     <li><a href="/home#resume">Resume</a></li>
                     <!-- Dropdown Trigger -->
                     <li><a class="dropdown-button" href="#!" data-activates="dropdownJava">Projects<i class="material-icons right">arrow_drop_down</i></a></li>
                 </ul>
             </div>
-          </nav>
+            </nav>
+            <!-- for mobile view -->
+            <ul class="side-nav" id="mobile">
+                <li><a class="purple-text"href="/home#contact">Contact Me</a></li>
+                <li><a class="purple-text"href="/home#resume">Resume</a></li>
+                <li class="divider"></li>
+                <li><a class="purple-text" href="{{{action('HomeController@showWizard')}}}">Wizard Vs Dragon</a></li>
+                <li><a class="purple-text" href="{{{action('HomeController@showButterfly')}}}">Catch A Butterfly</a></li>
+                <li><a class="purple-text" href="{{{action('HomeController@showBlog')}}}">Blog</a></li>
+                <li><a class="purple-text" href="{{{action('HomeController@showMore')}}}">More Projects</a></li>
+            </ul>
 
     @yield('splash')
 
