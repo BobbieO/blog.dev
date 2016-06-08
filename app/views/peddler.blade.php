1ce2e59e173191fb8983d76806a992ffb6d1b4fe
BobbieO <bobbie2504@msn.com>
2016-06-08 17:58:59 -0500

    Reverted peddler page back to explanation link view in prep for deployment of peddler site. Removed gallery and gallery's js.

--- app/views/peddler.blade.php
+++ app/views/peddler.blade.php
@@ -23,46 +23,22 @@
 
         <div>
             <div class="container row">
-                <div class="col s12">
-                    <h3 class="center-align">Screenshot Gallery</h3>
+                <div class="col s6 instrImg">
+                <h3 class="center-align">Instructions</h3>
+                    <ol>
+                        <li>Explore the site.</li>
+                        <li>Admire the clever ads.</li>
+                        <li>Create your own user and ad.</li>
+                    </ol>
+                </div>
+                <div class="col s6 instrImg">
+                    <h3 class="center-align">Click to View</h3>
+                    <div class="center-align">
+                        <a href="#" target="_blank"><img class="image" src="#"></a>
+                    </div>
                 </div>
             </div>
             
-            <div class="slider row center-align">
-            <div class="col s8 col offset-s2">
-                <ul class="slides">
-                    <li>
-                        <img src="http://lorempixel.com/580/250/nature/1"> <!-- random image -->
-                        <div class="caption center-align">
-                            <h3>This is our big Tagline!</h3>
-                            <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
-                        </div>
-                    </li>
-                    <li>
-                        <img src="http://lorempixel.com/580/250/nature/2"> <!-- random image -->
-                        <div class="caption left-align">
-                            <h3>Left Aligned Caption</h3>
-                            <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
-                        </div>
-                    </li>
-                    <li>
-                        <img src="http://lorempixel.com/580/250/nature/3"> <!-- random image -->
-                        <div class="caption right-align">
-                            <h3>Right Aligned Caption</h3>
-                            <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
-                        </div>
-                    </li>
-                    <li>
-                        <img src="http://lorempixel.com/580/250/nature/4"> <!-- random image -->
-                        <div class="caption center-align">
-                            <h3>This is our big Tagline!</h3>
-                            <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
-                        </div>
-                    </li>
-                </ul>
-            </div>    
-            </div>
-  
             <div class="container row">
                 <div class="center-align col s12">
                     <a class="purple-text" href="https://github.com/Peddlers-Corner/adlister.dev" target="_blank">View code on GitHub</a>
--- public/js/home.js
+++ public/js/home.js
@@ -3,15 +3,7 @@
 $(document).ready(function(){ 
     $(".button-collapse").sideNav();
     $(".dropdown-button").dropdown();
-    // Pause slider
-    $('.slider').slider('pause');
-    // Start slider
-    $('.slider').slider('start');
-    // Next slide
-    $('.slider').slider('next');
-    // Previous slide
-    $('.slider').slider('prev');
-
+    
     // add an event listener for the delete button
     $('#delete-btn').on('click', function(e){
         // make sure the user wants to delete the post
