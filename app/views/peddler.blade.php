823d74de841c3699b21ef59139a76ab79f18b1f4
BobbieO <bobbie2504@msn.com>
2016-06-08 21:43:40 -0500

    Revised peddler explanation page, added screen shot image. Deleted js slider/carousel code from js file. No longer used.

--- app/views/peddler.blade.php
+++ app/views/peddler.blade.php
@@ -5,7 +5,6 @@
 @stop
 
 @section('content')
-
     <main>
         <div class="center-align container">
             <div class="right">
@@ -17,7 +16,7 @@
                 Peddler's Corner is full-stack modified Craigslist clone completed in a group of three people as part of the Codeup program. The sites uses Twitter Bootstrap and CSS for styling, and PHP and MySQL for functionality and database management.
             </p>
             <p>
-                This application allows for both registered and non-registered users to browse the full site, including individual items's pages as well as the full index of available services and merchandise. Only registered users may post items for sale. The site features user sign up, login, and logout, as well as an administrative user who has powers over the whole site. Users may create, edit, and delete thier own ads, and the administrator can delete any user's ad. 
+                This application allows for both registered and non-registered users to browse the full site, including individual items pages as well as the full index of available services and merchandise. Only registered users may post items for sale. The site features user sign up, login, and logout, as well as an administrative user who has powers over the whole site. Users may create, edit, and delete thier own ads, and the administrator can delete any user's ad. 
             </p>
         </div>
 
@@ -34,7 +33,7 @@
                 <div class="col s6 instrImg">
                     <h3 class="center-align">Click to View</h3>
                     <div class="center-align">
-                        <a href="#" target="_blank"><img class="image" src="#"></a>
+                        <a href="#" target="_blank"><img class="image" src="/img/PCHomeScreenShot.jpg"></a>
                     </div>
                 </div>
             </div>
@@ -44,7 +43,7 @@
                     <a class="purple-text" href="https://github.com/Peddlers-Corner/adlister.dev" target="_blank">View code on GitHub</a>
                 </div>
             </div>
-        </div>
 
+        </div>
     </main>
 @stop
--- public/js/home.js
+++ public/js/home.js
@@ -3,7 +3,7 @@
 $(document).ready(function(){ 
     $(".button-collapse").sideNav();
     $(".dropdown-button").dropdown();
-    
+
     // add an event listener for the delete button
     $('#delete-btn').on('click', function(e){
         // make sure the user wants to delete the post
