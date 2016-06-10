ecbe36fb9beb75916605d5ae3bf8622f7edc7eeb
BobbieO <bobbie2504@msn.com>
2016-06-10 10:44:09 -0500

    Edited typo, added specific responsibility.

--- app/views/peddler.blade.php
+++ app/views/peddler.blade.php
@@ -13,10 +13,10 @@
             </div>
             <h1 class="down">Peddler's Corner</h1>
             <p>
-                Peddler's Corner is full-stack modified Craigslist clone completed in a group of three people as part of the Codeup program. The sites uses Twitter Bootstrap and CSS for styling, and PHP and MySQL for functionality and database management.
+                Peddler's Corner is full-stack modified Craigslist clone completed in a group of three people as part of the Codeup program. The sites uses Twitter Bootstrap and CSS for styling, and PHP and MySQL for functionality and database management. I was responsible for site design and user interface, as well as sharing back-end and database tasks.
             </p>
             <p>
-                This application allows for both registered and non-registered users to browse the full site, including individual items pages as well as the full index of available services and merchandise. Only registered users may post items for sale. The site features user sign up, login, and logout, as well as an administrative user who has powers over the whole site. Users may create, edit, and delete thier own ads, and the administrator can delete any user's ad. 
+                This application allows for both registered and non-registered users to browse the full site, including individual items pages as well as the full index of available services and merchandise. Only registered users may post items for sale. The site features user sign up, login, and logout, as well as an administrative user who has powers over the whole site. Users may create, edit, and delete their own ads, and the administrator can delete any user's ad. 
             </p>
         </div>
 
