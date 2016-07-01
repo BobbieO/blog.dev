dd7b4cd99efaa87fe401016f5a9b2692160cca7b
BobbieO <bobbie2504@msn.com>
2016-07-01 13:06:13 -0500

    Edited pathing to new showcase page. Edited typo in body.

--- app/views/peddler.blade.php
+++ app/views/peddler.blade.php
@@ -9,11 +9,11 @@
         <div class="center-align container">
             <div class="right">
                 <a href="{{{action('HomeController@showBlog')}}}"><i class="nav-arrow medium material-icons">navigate_before</i></a>
-                <a href="{{{action('HomeController@showMore')}}}"><i class="nav-arrow medium material-icons">navigate_next</i></a>
+                <a href="{{{action('HomeController@showFlit')}}}"><i class="nav-arrow medium material-icons">navigate_next</i></a>
             </div>
             <h1 class="down">Peddler's Corner</h1>
             <p>
-                Peddler's Corner is full-stack modified Craigslist clone completed in a group of three people as part of the Codeup program. The sites uses Bootstrap and CSS for styling, and PHP and MySQL for functionality and database management. I was responsible for site design and user interface, as well as sharing back-end and database tasks.
+                Peddler's Corner is full-stack modified Craigslist clone completed in a group of three people as part of the Codeup program. The site uses Bootstrap and CSS for styling, and PHP and MySQL for functionality and database management. I was responsible for site design and user interface, as well as sharing back-end and database tasks.
             </p>
             <p>
                 This application allows for both registered and non-registered users to browse the full site, including individual items pages as well as the full index of available services and merchandise. Only registered users may post items for sale. The site features user sign up, login, and logout, as well as an administrative user who has powers over the whole site. Users may create, edit, and delete their own ads, and the administrator can delete any user's ad. 
